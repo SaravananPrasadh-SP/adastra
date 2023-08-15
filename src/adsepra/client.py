fd84a69bdc5befc6302ab21335c204dcb04c988d
Arne Ottens <arne.ottens@starburst.io>
2023-08-15 14:30:07 +0200

    Introduced custom session to intercept certain HTTP Status

--- src/adsepra/client.py
+++ src/adsepra/client.py
@@ -3,22 +3,33 @@ from pydantic import BaseModel, PrivateAttr
 from requests import Session
 
 
+class SepSession(Session):
+
+    def __init__(self, user: str, token: str):
+        super().__init__()
+        self.headers.update({
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': token,
+            'X-Trino-User': user
+        })
+
+    def request(self, method, url, **kwargs):
+        res = super().request(method, url, **kwargs)
+        if res.status_code not in [200, 404, 405]:
+            res.raise_for_status()
+        return res
+
+
 class SepClient(BaseModel):
     host: str
     user: str
     token: str
-    _client: Session = PrivateAttr()
+    _client: SepSession = PrivateAttr()
 
     def __init__(self, **data):
         super().__init__(**data)
-        session = requests.Session()
-        session.headers.update({
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'Authorization': self.token,
-            'X-Trino-User': self.user
-        })
-        self._client = session
+        self._client = SepSession(self.user, self.token)
 
     def get(self, endpoint: str, params=None):
         return self._client.get(self.host + endpoint, params=params)
